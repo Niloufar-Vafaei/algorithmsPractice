8614a1a5d0588315c393fbe35590e6827253e682
Niloufar-Vafaei <Niloufar.vafaeie@gmail.com>
2020-12-09 16:40:51 -0500

    add new algorithm

---
+++ src/com/imp/DistnationCity.java
@@ -0,0 +1,20 @@
+package com.imp;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DistnationCity {
+    public String destCity(List<List<String>> paths) {
+        Set<String> startPoint = new HashSet<>();
+        String result="";
+        for (List<String> each : paths) {
+            startPoint.add(each.get(0));
+        }
+        for (List<String> each : paths) {
+            if (!startPoint.contains(each.get(1)))
+                result = each.get(1);
+        }
+        return result;
+    }
+}
