bbcf5e79dc282be4b30d2fdd32965a2afca3e5b8
Niloufar-Vafaei <Niloufar.vafaeie@gmail.com>
2020-12-10 10:30:52 -0500

    check change user

--- src/com/imp/FlippingImage.java
+++ src/com/imp/FlippingImage.java
@@ -1,4 +1,11 @@
 package com.imp;
+/*
+Given a binary matrix A, we want to flip the image horizontally, then invert it, and return the resulting image.
 
+ */
 public class FlippingImage {
+    public int[][] flipAndInvertImage(int[][] A) {
+        return null;
+
+    }
 }
